1be0a548177dc1dc1ed6c12b5662566d563d739a
Karera <neliusm4@gmail.com>
2023-08-31 18:39:38 +0300

    domain

--- church/settings.py
+++ church/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG','True').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['blogy-production.up.railway.app','127.0.0.1']
 
 
 # Application definition
